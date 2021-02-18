8597e787e5078ba2a214f6a1daa13004919901a8
Apan Jain <apanjain3@gmail.com>
2021-02-18 13:29:25 +0530

    Create Basic layout of CreateBlog view

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -6,10 +6,88 @@ class CreateBlog extends StatefulWidget {
 }
 
 class _CreateBlogState extends State<CreateBlog> {
+  String authorName, title, desc;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              "Blog",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.file_upload),
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: Icon(
+                Icons.add_a_photo,
+                color: Colors.black45,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(hintText: "Author Name"),
+                    onChanged: (value) {
+                      authorName = value;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: "Title"),
+                    onChanged: (value) {
+                      title = value;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: "Description"),
+                    onChanged: (value) {
+                      desc = value;
+                    },
+                  ),
+                ],
+              )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
