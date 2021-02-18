fc7cb2341aa186f9edeb890684930f848515cf28
Apan Jain <apanjain3@gmail.com>
2021-02-18 15:59:15 +0530

    Setup image picker

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreateBlog extends StatefulWidget {
   @override
@@ -7,6 +10,22 @@ class CreateBlog extends StatefulWidget {
 
 class _CreateBlogState extends State<CreateBlog> {
   String authorName, title, desc;
+
+  File selectedImage;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        selectedImage = File(pickedFile.path);
+      } else {
+        print('No Image Selected');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,47 +63,64 @@ class _CreateBlogState extends State<CreateBlog> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Icon(
-                Icons.add_a_photo,
-                color: Colors.black45,
-              ),
+            GestureDetector(
+              onTap: () {
+                getImage();
+              },
+              child: selectedImage != null 
+                  ? Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 150,
+                      width: MediaQuery.of(context).size.width,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.file(
+                          selectedImage,
+                          fit: BoxFit.cover,
+                        )
+                      ),
+                  )
+                  : Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      child: Icon(
+                        Icons.add_a_photo,
+                        color: Colors.black45,
+                      ),
+                  ),
             ),
             SizedBox(
               height: 8,
             ),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(hintText: "Author Name"),
-                    onChanged: (value) {
-                      authorName = value;
-                    },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(hintText: "Title"),
-                    onChanged: (value) {
-                      title = value;
-                    },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(hintText: "Description"),
-                    onChanged: (value) {
-                      desc = value;
-                    },
-                  ),
-                ],
-              )
-            )
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(hintText: "Author Name"),
+                      onChanged: (value) {
+                        authorName = value;
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(hintText: "Title"),
+                      onChanged: (value) {
+                        title = value;
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(hintText: "Description"),
+                      onChanged: (value) {
+                        desc = value;
+                      },
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
