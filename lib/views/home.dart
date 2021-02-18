03f0b35d53f82b95f8d550737491fb9dc0521ad9
Apan Jain <apanjain3@gmail.com>
2021-02-18 13:06:32 +0530

    Create home screen and basic navigation

---
+++ lib/views/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/views/create_blog.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              "Blog",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(),
+      floatingActionButton: Container(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CreateBlog()));
+              },
+              child: Icon(Icons.add),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
